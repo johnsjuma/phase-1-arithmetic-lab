52cabbe1f75a19dcb076dbcbbc7d1d82b954985f
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-06 22:06:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+console.log(
+    var1*var2
+)
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(
+    num3%num4
+)
