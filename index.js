0724e4967c0431573208db88fb7c7bb022534fd4
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-07 09:29:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-
-// Write your code here
-const var1 = 2
-const var2 = 31
-const multiply = var1 * var2
-console.log(
-    var1*var2
-)
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor( Math.random() * 1000 ) + 1;
 const num3 = 16;
 const num4 = 6;
 const mod = num3 % num4;
-console.log(
-    num3%num4
-)
+const max = Math.max( 5, 10, 15, 20 );
